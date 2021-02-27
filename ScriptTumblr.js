e800b85b6bbd1312e51d77235bbf7b3b4c7a232f
Kay <76020372+SiIentMoon@users.noreply.github.com>
2021-02-27 17:55:07 -0300

    ScriptTumblr.js

---
+++ ScriptTumblr.js
@@ -0,0 +1,7 @@
+/* script by Kay * SiIentMoon.tumblr.com *  2021 (c) All Rights Reserved.*/
+
+!function(s){var i=s(window),t=s(".wrapper").children("li").each(function(){0<s(this).next("li").length&&s("<li><span></span></li>").insertAfter(s(this))}),s("ul.chat").children("li").each(function(){s(this).find(".label").each(function(){var t=s(this).text().replace(":","");s(this).text(t)})}
+
+jQuery(document).ready(function()
+
+});
