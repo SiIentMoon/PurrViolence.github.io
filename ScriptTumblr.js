f9ecbd10b1d86762a9170331cfebf09513515c66
Kay <76020372+SiIentMoon@users.noreply.github.com>
2021-02-28 20:55:20 -0300

    Update ScriptTumblr.js

--- ScriptTumblr.js
+++ ScriptTumblr.js
@@ -5,3 +5,31 @@
 jQuery(document).ready(function()
 
 });
+             
+//almacenando el div y el boton en unas variables
+var div = document.getElementById('tagscontent');
+var but = document.getElementById('onmeuwu');
+
+//la funcion que oculta y muestra
+function showHide(e){
+e.preventDefault();
+e.stopPropagation();
+if(div.style.display == "none"){
+div.style.display = "block";
+} else if(div.style.display == "block"){
+div.style.display = "none";
+}
+}
+//al hacer click en el boton
+but.addEventListener("click", showHide, false);
+
+//funcion para cualquier clic en el documento
+document.addEventListener("click", function(e){
+console.log('clic');
+//obtiendo informacion del DOM para  
+var clic = e.target;
+console.log(clic);
+if(div.style.display == "block" && clic != div){
+ div.style.display = "none";
+}
+}, false);
