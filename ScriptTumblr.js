14661b622551dbe5dc5baee0bc0080e9e5f082c7
Kay <76020372+SiIentMoon@users.noreply.github.com>
2021-02-28 21:56:57 -0300

    Update ScriptTumblr.js

--- ScriptTumblr.js
+++ ScriptTumblr.js
@@ -2,11 +2,7 @@
 
 !function(s){var i=s(window),t=s(".wrapper").children("li").each(function(){0<s(this).next("li").length&&s("<li><span></span></li>").insertAfter(s(this))}),s("ul.chat").children("li").each(function(){s(this).find(".label").each(function(){var t=s(this).text().replace(":","");s(this).text(t)})}
 
-jQuery(document).ready(function()
 
-});
-             
-             
 
 $(function(){
     //oculta al hacer click fuera del elemnto (formulario)
